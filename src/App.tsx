57813f68ac65f1a5691adb233be1d6b25dcdcfa6
Jorge Saa <piojo.sistematico@gmail.com>
2023-10-16 20:29:53 -0300

    basic html markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,108 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import logo from "./assets/images/logo.svg";
+import cart from "./assets/images/icon-cart.svg";
+import avatar from "./assets/images/image-avatar.png";
+import shoes from "./assets/images/image-product-1.jpg";
 
+import iconPlus from "./assets/images/icon-plus.svg";
+import iconMinus from "./assets/images/icon-minus.svg";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <picture>
+          <img src={logo} alt="" />
+          <ul>
+            <li>
+              <a href="#">Collections</a>
+            </li>
+            <li>
+              <a href="#">Men</a>
+            </li>
+            <li>
+              <a href="#">Women</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+          <div>
+            <a href="#">
+              <img src={cart} alt="" />
+            </a>
+            <a href="#">
+              <img src={avatar} alt="" />
+            </a>
+          </div>
+        </picture>
+        <main>
+          <section title="slider">
+            <picture>
+              <img src={shoes} alt="" />
+            </picture>
+            <ul>
+              <li>
+                <button>
+                  <img src={shoes} alt="" />
+                </button>
+              </li>
+              <li>
+                <button>
+                  <img src={shoes} alt="" />
+                </button>
+              </li>
+              <li>
+                <button>
+                  <img src={shoes} alt="" />
+                </button>
+              </li>
+              <li>
+                <button>
+                  <img src={shoes} alt="" />
+                </button>
+              </li>
+            </ul>
+          </section>
+          <section title="details">
+            <h2>Sneaker Company</h2>
+            <h1>Fall Limited Edition Sneakers</h1>
+            <p>
+              These low-profile sneakers are your perfect casual wear companion.
+              Featuring a durable rubber outer sole, they’ll withstand
+              everything the weather can offer.
+            </p>
+            <div>
+              <div>
+                <span>$125.00</span>
+                <span>50%</span>
+              </div>
+              <span> $250.00</span>
+            </div>
+            <div>
+              <div>
+                <button>
+                  <img src={iconMinus} alt="" />
+                </button>
+
+                <span>0</span>
+                <button>
+                  <img src={iconPlus} alt="" />
+                </button>
+              </div>
+              <button>
+                <img src={cart} alt="" /> <span>Add to cart</span>
+              </button>
+            </div>
+          </section>
+        </main>
+      </header>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
